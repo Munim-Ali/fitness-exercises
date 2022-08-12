8631a26805388278536d5726f72d000f5fa9adf5
Munim_Ali <munimali52@gmail.com>
2022-08-12 17:51:21 +0500

    First Commit

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useState } from 'react'
+import { Box } from '@mui/material'
+import HeroBanner from '../componenets/HeroBanner'
+import SearchExercises from '../componenets/SearchExercises'
+import Exercises from '../componenets/Exercises'
+
+const Home = () => {
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises />
+      <Exercises />
+    </Box>
+  )
+}
+
+export default Home
